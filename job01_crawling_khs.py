df5c8cd81f0201b4c1c0f429feffe0aff81234a9
kimnokdu <rlagustjq12@gmail.com>
2021-12-02 14:56:16 +0900

    12월 2일 코드

--- job01_crawling_khs.py
+++ job01_crawling_khs.py
@@ -4,7 +4,7 @@ from selenium.common.exceptions import NoSuchElementException
 import time
 
 options = webdriver.ChromeOptions()
-# options.add_argument('headless')   ## 이옵션이면 브라우저가 안뜸
+options.add_argument('headless')   ## 이옵션이면 브라우저가 안뜸
 options.add_argument('lang=ko_KR')
 options.add_argument('disable_gpu')
 
@@ -13,16 +13,69 @@ driver = webdriver.Chrome('./chromedriver', options= options)
 titles = []
 reviews = []
 
-# https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open=2020
-# https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open=2020&page=4
+# https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open=2020&page=1
+# https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open=2020&page=2   37까지
+# 영화 제목 xpath
+# //*[@id="old_content"]/ul/li[1]/a
+# //*[@id="old_content"]/ul/li[2]/a
+# //*[@id="old_content"]/ul/li[20]/a
+# //*[@id="movieEndTabMenu"]/li[6]/a/em  리뷰버튼,
+# //*[@id="reviewTab"]/div/div/div[2]/span/em 리뷰 건수
 
-for i in range(1, 38):
-    url = 'https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open=2020&page={}'.format(i)
-    for j in range(1, 21):
-        try:
-            driver.get(url)
-            movie_title_xpath = '//*[@id="old_content"]/ul/li[{}]/a'.format(j)
-            title = driver.find_element_by_xpath(movie_title_xpath).text
-            print(title)
-        except:
-            print('error')
+# //*[@id="pagerTagAnchor1"]   리뷰 페이지 버튼
+# //*[@id="pagerTagAnchor10"]/em   리뷰 다음 페이지 버튼
+# //*[@id="reviewTab"]/div/div/ul/li[1]/a/strong 리뷰 제목
+# //*[@id="SE-ec9bce5c-9be3-47a9-9957-b075426d88fb"] 리뷰 한 줄
+# //*[@id="content"]/div[1]/div[4]/div[1]/div[4]        # class:user_tx_area
+
+
+review_button_xpath = '//*[@id="movieEndTabMenu"]/li[6]/a/em'
+review_number_xpath = '//*[@id="reviewTab"]/div/div/div[2]/span/em'
+try:
+    for i in range(1, 38):
+        url = 'https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open=2020&page={}'.format(i)
+        titles = []
+        reviews = []
+        for j in range(1, 21):
+            print(j+((i-1)*20), '번째 영화 크롤링 중')
+            try:
+                driver.get(url)
+                movie_title_xpath = '//*[@id="old_content"]/ul/li[{}]/a'.format(j)
+                title = driver.find_element_by_xpath(movie_title_xpath).text
+                driver.find_element_by_xpath(movie_title_xpath).click()
+                driver.find_element_by_xpath(review_button_xpath).click()
+                review_range = driver.find_element_by_xpath(review_number_xpath).text.replace(',', '')
+                review_range = int(review_range)
+                review_range = review_range // 10 + 2
+                if review_range > 6: review_range = 6
+                for k in range(1, review_range):
+                    review_page_url = driver.find_element_by_xpath('//*[@id="movieEndTabMenu"]/li[6]/a').get_attribute('href')
+                    driver.get(review_page_url + '&page={}'.format(k))
+                    time.sleep(0.3)
+                    for l in range(1, 11):
+                        review_title_xpath = '//*[@id="reviewTab"]/div/div/ul/li[{}]/a/strong'.format(l)
+                        try:
+                            driver.find_element_by_xpath(review_title_xpath).click()
+                            time.sleep(0.3)
+                            review = driver.find_element_by_xpath('//*[@id="content"]/div[1]/div[4]/div[1]/div[4]').text
+                            # print('===================== =====================')
+                            print(title)
+                            # print(review)
+                            reviews.append(review)
+                            titles.append(title)
+                            driver.back()
+                        except:
+                            print(l, '번째 review가 없다.')
+                            break
+            except:
+                print('error')
+        df_review_20 = pd.DataFrame({'title': titles , 'review':reviews})
+        df_review_20.to_csv('./crawling_data/reviews_{}_{}.csv'.format(2020 , i) , index = False)
+except:
+    print('totally error')
+finally:
+    driver.close()
+
+
+# df_review = pd.DataFrame({'title': titles , 'review':reviews})
+# df_review.to_csv('./crawling_data/reviews_{}.csv'.format(2020))

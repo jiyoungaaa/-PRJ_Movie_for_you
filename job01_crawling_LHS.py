98d880eeed8538eb75ad5f9799f5daa4cb7953f6
Hyeoun <ing02084@naver.com>
2021-12-02 14:49:30 +0900

    movie review crawling 02

--- job01_crawling_LHS.py
+++ job01_crawling_LHS.py
@@ -11,9 +11,6 @@ options.add_argument('disable_gpu')
 
 driver = webdriver.Chrome('./chromedriver', options=options)
 
-titles = []
-reviews = []
-
 # https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open=2020&page=1
 # https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open=2020&page=2  37까지
 # 영화 제목
@@ -33,32 +30,52 @@ reviews = []
 review_button_xpath = '//*[@id="movieEndTabMenu"]/li[6]/a'
 review_number_xpath = '//*[@id="reviewTab"]/div/div/div[2]/span/em'
 
-for i in range(1, 38):
-    url = 'https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open=2020&page={}'.format(i)
-    driver.get(url)
-    for j in range(1, 21):
-        try:
-            movie_title_xpath = '//*[@id="old_content"]/ul/li[{}]/a'.format(j)
-            title = driver.find_element_by_xpath(movie_title_xpath).text
-            print(title)
-            titles.append(title)
-            driver.find_element_by_xpath(movie_title_xpath).send_keys(Keys.ENTER)
-            driver.find_element_by_xpath(review_button_xpath + '/em').click()
-            review_range = driver.find_element_by_xpath(review_number_xpath).text
-            review_range = int(review_range.replace(',', '')) // 10 + 2
-            for k in range(1, review_range):
+try:
+    for i in range(25, 38):
+        url = 'https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open=2020&page={}'.format(i)
+        titles = []
+        reviews = []
+        for j in range(1, 21):
+            print(j + ((i - 1) * 20), '번째 영화 크롤링 중....')
+            try:
+                driver.get(url)
+                time.sleep(0.1)
+                movie_title_xpath = '//*[@id="old_content"]/ul/li[{}]/a'.format(j)
+                title = driver.find_element_by_xpath(movie_title_xpath).text
+                driver.find_element_by_xpath(movie_title_xpath).send_keys(Keys.ENTER)
+                time.sleep(0.1)
                 review_page_url = driver.find_element_by_xpath(review_button_xpath).get_attribute('href')
-                driver.get(review_page_url + '&Page={}'.format(k))
-                for l in range(1, 11):
-                    review_title_xpath = '//*[@id="reviewTab"]/div/div/ul/li[{}]/a/strong'.format(l)
-                    try:
-                        driver.find_element_by_xpath(review_title_xpath).click()
-                        review = driver.find_element_by_xpath('//*[@id="content"]/div[1]/div[4]/div[1]/div[4]').text
-                        print('========================')
-                        print(review)
-                        driver.back()
-                    except:
-                        driver.get(url)
-                        break
-        except:
-            print('error')
+                driver.get(review_page_url)
+                time.sleep(0.1)
+                review_range = driver.find_element_by_xpath(review_number_xpath).text
+                review_range = int(review_range.replace(',', '')) // 10 + 2
+                if review_range > 6: review_range = 6
+                for k in range(1, review_range):
+                    driver.get(review_page_url + '&Page={}'.format(k))
+                    time.sleep(0.1)
+                    for l in range(1, 11):
+                        review_title_xpath = '//*[@id="reviewTab"]/div/div/ul/li[{}]/a/strong'.format(l)
+                        try:
+                            driver.find_element_by_xpath(review_title_xpath).click()
+                            time.sleep(0.1)
+                            review = driver.find_element_by_xpath('//*[@id="content"]/div[1]/div[4]/div[1]/div[4]').text
+                            # print('===========================  ===============================')
+                            # print(title)
+                            # print(review)
+                            titles.append(title)
+                            reviews.append(review)
+                            driver.back()
+                        except:
+                            print(l, '번째 리뷰가 없다.')
+                            # driver.get(url)
+                            break
+            except:
+                print('error')
+        df_review_20 = pd.DataFrame({'title': titles, 'reviews': reviews})
+        df_review_20.to_csv('./crawling_data/reviews_{}_{}.csv'.format(2020, i), index=False)
+except:
+    print('totally error')
+finally:
+    driver.close()
+# df_review = pd.DataFrame({'title':titles, 'reviews':reviews})
+# df_review.to_csv('./crawling_data/reviews_{}.csv'.format(2020))
